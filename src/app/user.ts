7033eee55afd68fa6ee4e2a91c30aadf847bf40e
ynyanchoka <monaryymelda@gmail.com>
2022-04-15 13:15:13 +0300

    add class user

---
+++ src/app/user.ts
@@ -0,0 +1,14 @@
+export class User {
+    constructor(
+        public url:string,
+        public login:string, 
+        public html_url:string,
+        public location:string, 
+        public public_repos:number,
+        public followers:number,
+        public following:number,
+        public avatar_url:string,
+        public created_at:Date ){
+   
+     }
+}
